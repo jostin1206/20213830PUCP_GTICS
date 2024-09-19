7114c73c56251d4990d24933a369dd4bc19ebccf
Jostin <a20213830@pucp.edu.pe>
2024-09-18 20:08:59 -0500

    commit

---
+++ src/main/java/com/example/lab4/entity/Flores.java
@@ -0,0 +1,16 @@
+package com.example.lab4.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Flores {
+
+    private int idFlores;
+    private String nombre;
+    private Double precio;
+
+    private String descripcion;
+
+}
